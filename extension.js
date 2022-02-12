44cae4c647b85224960b1fbf611a29066ea29ee3
nickmccaigntu <nick.mccaig2019@my.ntu.ac.uk>
2022-02-12 14:13:01 +0000

    Added some consts and other stuf
    Co-Authored-By: Hannah Ashna Jacob <hannah.ashna.jacob@gmail.com>

--- extension.js
+++ extension.js
@@ -2,7 +2,16 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const TUI = require('./UI-functions.js');
+const  EmotionImages = {
+	"happy": "https://frograts.github.io//HackNotts2021/tackyHappy.png",
+	"sad": "https://avatars.githubusercontent.com/u/56484022?v=4",
+	"angry":""
+  };
 
+const Responses = {
+	"changeTheme": "Hi there! Looks like you're having a tough time with your coding ... let me help!",
+	"changeThemeNo": "Too bad ;)"
+};
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -16,60 +25,53 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tacky-the-thumbtack" is now active!');
 	const panel = vscode.window.createWebviewPanel(
-		'catCoding',
-		'Cat Coding',
+		'tackyChat',
+		'Tacky Chat',
 		vscode.ViewColumn.Two,
 		{}
 	  );
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tacky-the-thumbtack.helloWorld', function () {
+
+	// Function -- Init Tacky
+	let disposable = vscode.commands.registerCommand('tacky-the-thumbtack.awakenTacky', function () {
 		// The code you place here will be executed every time your command is executed
 		const updateWebview = () => {
-			panel.webview.html = TUI.getWebviewContent('https://avatars.githubusercontent.com/u/56484022?v=4', "Hannah T ;)");
+			panel.webview.html = TUI.getWebviewContent(EmotionImages['happy'], "Hannah T ;)");
 		  };
 	
-		  // Set initial content
-		  updateWebview();
+		 // Set initial content
+		updateWebview();
+
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Tacky The Thumbtack!');
 
 	});
 
-	// Function -- Start
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(
-		vscode.commands.registerCommand('tacky-the-thumbtack.start', () => {
-		  
-			
-		  const updateWebview = () => {
-			panel.webview.html = TUI.getWebviewContent('https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif', "HelloFrom T ;)");
-		  };
-	
-		  // Set initial content
-		  updateWebview();
-	
-		  // And schedule updates to the content every second
-		  //setInterval(updateWebview, 1000);
-		})
-	  );
-
 	// Function -- Lightmode
 	context.subscriptions.push(
-		vscode.commands.registerCommand("tacky-the-thumbtack.askQuestion", async () => {
+		vscode.commands.registerCommand("tacky-the-thumbtack.changeTheme", async () => {
+			const updateWebview = () => {
+			panel.webview.html = TUI.getWebviewContent(EmotionImages['sad'], Responses['changeTheme']);
+			};
+		
+			 // Set initial content
+			updateWebview();
+
 			const answer = await vscode.window.showInformationMessage(
-			  "How was your day?",
-			  "Good!",
-			  "Bad :("
+			  "Will you let me help?",
+			  "Yes",
+			  "No"
 			);
 	  
-			if (answer === "Bad :(") {
-			  	vscode.window.showInformationMessage("Sorry to hear that $(alert)");
+			if (answer === "No") {
+				const updateWebview = () => {
+					panel.webview.html = TUI.getWebviewContent(EmotionImages['sad'], Responses['changeThemeNo']);
+				};
+				
+				// Set initial content
+				updateWebview();
 				vscode.workspace.getConfiguration().update("workbench.colorTheme", "Solarized Light");
 				
 			} else {
-				vscode.window.showErrorMessage("TEST")
 				vscode.workspace.getConfiguration().update("workbench.colorTheme", "Default Dark+");
 			}
 		  })
