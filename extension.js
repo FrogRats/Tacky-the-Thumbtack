1914b76a50a49b4210d5175b6e760931b8db6e8b
nickmccaigntu <nick.mccaig2019@my.ntu.ac.uk>
2022-02-12 17:01:40 +0000

    Updated images and added on delete function

--- extension.js
+++ extension.js
@@ -3,14 +3,15 @@
 const vscode = require('vscode');
 const TUI = require('./UI-functions.js');
 const  EmotionImages = {
-	"happy": "https://frograts.github.io//HackNotts2021/tackyHappy.png",
-	"sad": "https://avatars.githubusercontent.com/u/56484022?v=4",
-	"mad": "https://frograts.github.io//HackNotts2021/tackyMad.png"
+	"happy": "https://frograts.github.io//HackNotts2021/tackyHappy.gif",
+	"sad": "https://frograts.github.io//HackNotts2021/tackySad.gif",
+	"mad": "https://frograts.github.io//HackNotts2021/tackyMad.gif"
   };
 const Responses = {
     "changeTheme": "Hi there! Looks like you're having a tough time with your coding ... let me help!",
     "changeThemeNo": "Too bad ;)",
-	"FileCreation": "Adding to your project? dont forget to your Readme <3"};
+	"FileCreation": "Adding to your project? dont forget to your Readme <3",
+	"FileDeleation": "Where did the files go ?"};
 
 const TF = require('./Trilio-Functions.js');
 const { Console } = require('console');
@@ -39,7 +40,7 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('tacky-the-thumbtack.awakenTacky', function () {
 		// The code you place here will be executed every time your command is executed
 		const updateWebview = () => {
-			panel.webview.html = TUI.getWebviewContent(EmotionImages['happy'], "Hannah T ;)");
+			panel.webview.html = TUI.getWebviewContent(EmotionImages['happy'], "Hi! I'm looking forward to helping you ;)");
 		  };
 	
 		 // Set initial content
@@ -106,9 +107,15 @@ function activate(context) {
 		vscode.window.showInformationMessage('Pay attention to Tacky ... :(');
 	})
 	vscode.workspace.onDidCreateFiles(async () => {
-		if(Math.floor(Math.random() * 11)  >= 3){
-		console.log("???")
+		if(Math.floor(Math.random() * 11)  >= 7){
+		console.log("Create event")
 		panel.webview.html = TUI.getWebviewContent(EmotionImages["happy"],Responses["FileCreation"]);
+		}
+	})
+	vscode.workspace.onDidDeleteFiles(async () => {
+		if(Math.floor(Math.random() * 11)  >= 7){
+		console.log("Del event")
+		panel.webview.html = TUI.getWebviewContent(EmotionImages["mad"],Responses["FileDeleation"]);
 		}
 	})
 	
