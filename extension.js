33427d509e7ccbea65000514f4e366969f5655a9
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2022-02-12 11:55:44 +0000

    Make separate functions file

--- extension.js
+++ extension.js
@@ -73,19 +73,4 @@ module.exports = {
 	activate,
 	deactivate
 }
-/**
- * @param {string} cats
- */
-function getWebviewContent(cats) {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="${cats}" width="300" />
-  </body>
-  </html>`;
-  }
+
